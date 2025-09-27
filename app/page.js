11480ee5d0afda42da07b71b979eb98ebcf0cb7e
hiracoh <takuyo.register@gmail.com>
2025-09-28 07:33:06 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,10 +8,11 @@ export default function Home() {
       </div>
 
       <h1 style={{ fontSize:'1.9rem', margin:0 }}>life atlas</h1>
-      <p style={{ margin:'0.25rem 0 0.75rem', color:'#555' }}>
+      <p style={{ margin:'0.25rem 0 0.75rem', color:'#555', whiteSpace:'pre-line'  }}>
         自分を生きる上での、地図のような場所を。
 
-        ここには地図があります。生きている地図です。日々新しく入れ替わるし、あなたの現在地合わせて形を変えます。
+        ここには地図があります。生きている地図です。
+        過去のものも書き変わる、日々新しいものが加わる、そしてあなたの現在地に合わせて形を変えます。
       </p>
 
       <div style={{ display:'flex', gap:'0.75rem', flexWrap:'wrap' }}>
